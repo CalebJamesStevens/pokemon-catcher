83a9e31cb8b492e5fc31564d0b826a605f9185c8
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-04-20 14:18:11 -0700

    Implemented react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inventory from './components/Inventory';
 
 function App() {
     return (
         <div className='App'>
-            <Inventory />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Inventory />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
